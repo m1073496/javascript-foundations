f1bdfcf7f8b8b607311852601343ca10596b4a03
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2020-12-08 12:16:31 -0700

    Complete dragon tests, update previous test files

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,28 @@
+var foodCount = 0;
 
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+  };
+
+  greet(rider) {
+    return `Hi, ${this.rider}!`;
+  };
+
+
+  eat() {
+    foodCount = foodCount + 1;
+    if (foodCount >= 3) {
+    this.hungry = false;
+  } else {
+    return this.hungry;
+  }
+  };
+
+};
+
+
+
+module.exports = Dragon;
