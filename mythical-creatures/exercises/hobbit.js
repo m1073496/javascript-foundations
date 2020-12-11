c8fb236902427b170bf8e01c5614797de29ade0b
Jessica Justice <jessica@jessicas-MacBook-Pro.local>
2020-12-10 17:51:50 -0700

    Complete hobbit tests

---
+++ mythical-creatures/exercises/hobbit.js
@@ -0,0 +1,43 @@
+class Hobbit {
+  constructor(hobbit) {
+    this.name = hobbit.name;
+    this.age = 0;
+    this.adult = false;
+    this.old = false;
+    this.hasRing = false;
+  }
+
+  celebrateBirthday() {
+    this.age = this.age + 1;
+    if (this.age >= 33 && this.age < 101) {
+      this.adult = true;
+    } else if (this.age >= 101){
+      this.old = true;
+    }
+  };
+
+  getRing() {
+    if (this.name === 'Frodo') {
+      this.hasRing = true;
+      return "Here is the ring!"
+    } else {
+      return "You can't have it!"
+    }
+  };
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Hobbit;
